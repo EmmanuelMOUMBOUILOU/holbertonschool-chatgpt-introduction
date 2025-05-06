be0b92795a2a5829d1cc205fc3e8ff42a5fd430a
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-05-06 15:36:26 +0200

    Renommage de la fonction factorial en factorielle et amélioration des messages d'erreur pour une meilleure clarté.

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-
 import sys
-
-def factorial(n):
-    result = 1
+def factorielle(n):
+    resultat = 1
     while n > 1:
-        result *= n
-        n -= 1
-    return result
-
+        resultat *= n
+        n -= 1  # On décrémente n pour éviter une boucle infinie
+    return resultat
+# Vérifie qu'un argument a été fourni
 if len(sys.argv) != 2:
-    print("Usage: ./factorial.py <non-negative integer>")
+    print("Utilisation : ./factorial.py <nombre>")
     sys.exit(1)
-
 try:
-    number = int(sys.argv[1])
-    if number < 0:
-        f = factorial(number)
-        print(f)
-except ValueError as e:
-    print("Error:", e)
+    nombre = int(sys.argv[1])
+    if nombre < 0:
+        print("La factorielle n'est pas définie pour les nombres négatifs.")
+        sys.exit(1)
+    f = factorielle(nombre)
+    print(f)
+except ValueError:
+    print("Veuillez entrer un entier valide.")
     sys.exit(1)
